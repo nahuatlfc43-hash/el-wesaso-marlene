276f2c94f28409495d0f7bf56b7e620a93da4986
nahuatlfc43-hash <nahuatlfc43@gmail.com>
2026-07-23 07:52:47 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// 1. Identificamos las tarjetas por su ID
+const btnReportar = document.querySelector('#btn-reportar');
+const btnManual = document.querySelector('#btn-manual');
+
+// 2. Acción para el botón de Reportar
+btnReportar.addEventListener('click', () => {
+    alert("Iniciando sistema de reporte...");
+    // Aquí podrías llevarlos a otra página: 
+    // window.location.href = "reporte.html";
+});
+
+// 3. Acción para el botón de Manual
+btnManual.addEventListener('click', () => {
+    alert("¿Estás seguro de que quieres leer las 50 páginas del manual?");
+});
+
+
+
